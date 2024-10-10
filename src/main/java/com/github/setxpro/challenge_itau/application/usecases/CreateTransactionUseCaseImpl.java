e5f064bca043f44570be8f1edcfefe6b1dcfc921
Patrick Anjos <patrickpqdt87289@gmail.com>
2024-10-10 10:03:13 -0300

    refactor: validations extraction by method

--- src/main/java/com/github/setxpro/challenge_itau/application/usecases/CreateTransactionUseCaseImpl.java
+++ src/main/java/com/github/setxpro/challenge_itau/application/usecases/CreateTransactionUseCaseImpl.java
@@ -2,6 +2,7 @@ package com.github.setxpro.challenge_itau.application.usecases;
 
 import com.github.setxpro.challenge_itau.application.gateways.TransactionGateway;
 import com.github.setxpro.challenge_itau.domain.entities.Transaction;
+import com.github.setxpro.challenge_itau.domain.exceptions.FieldCannotBeEmptyException;
 import com.github.setxpro.challenge_itau.domain.exceptions.InvalidDateToTransactionException;
 import com.github.setxpro.challenge_itau.domain.exceptions.InvalidValueToTransactionException;
 
@@ -19,6 +20,19 @@ public class CreateTransactionUseCaseImpl implements CreateTransactionUseCase {
     @Override
     public Transaction execute(Transaction transaction) {
 
+        validateTransaction(transaction);
+
+        return transactionGateway.createTransaction(transaction);
+    }
+
+    private static void validateTransaction(Transaction transaction) {
+        if (transaction.getAmount() == null) {
+            throw new FieldCannotBeEmptyException("valor");
+        }
+        if (transaction.getDateHour() == null) {
+            throw new FieldCannotBeEmptyException("data");
+        }
+
         if (transaction.getAmount().compareTo(BigDecimal.ZERO) < 0) {
             throw new InvalidValueToTransactionException(transaction.getAmount());
         }
@@ -26,7 +40,5 @@ public class CreateTransactionUseCaseImpl implements CreateTransactionUseCase {
         if (transaction.getDateHour().isAfter(OffsetDateTime.now())) {
             throw new InvalidDateToTransactionException("Data inválida para transação.");
         }
-
-        return transactionGateway.createTransaction(transaction);
     }
 }
