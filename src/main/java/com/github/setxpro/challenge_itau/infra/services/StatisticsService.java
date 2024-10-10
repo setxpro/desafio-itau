5d8e1451e4309e00f83ffa86a6894c2678a1b3a8
Patrick Anjos <patrickpqdt87289@gmail.com>
2024-10-10 17:11:53 -0300

    feat: add performance tracking for transaction statistics using Micrometer Timer

--- src/main/java/com/github/setxpro/challenge_itau/infra/services/StatisticsService.java
+++ src/main/java/com/github/setxpro/challenge_itau/infra/services/StatisticsService.java
@@ -3,31 +3,43 @@ package com.github.setxpro.challenge_itau.infra.services;
 import com.github.setxpro.challenge_itau.domain.dtos.StatisticsDTO;
 import com.github.setxpro.challenge_itau.infra.strategies.StatisticsStrategy;
 import com.github.setxpro.challenge_itau.infra.strategies.StatisticsStrategyImpl;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
 
 @Service
+@Slf4j
 public class StatisticsService {
 
     private final StatisticsStrategy statistic;
+
+    @Autowired
+    private MeterRegistry meterRegistry;
 
     public StatisticsService(StatisticsStrategyImpl statistic) {
         this.statistic = statistic;
     }
 
     public StatisticsDTO statistic(String type, Integer time) {
+        return Timer.builder("transaction.statistics")  // Criação do timer para medir o tempo
+                .description("Tempo gasto para calcular estatísticas de transações")
+                .register(meterRegistry)
+                .record(() -> {
+                    final var now = OffsetDateTime.now();
+                    OffsetDateTime initialTime;
 
-        final var now = OffsetDateTime.now();
-        OffsetDateTime initialTime;
+                    switch (type) {
+                        case "seconds" -> initialTime = now.minusSeconds(time);
+                        case "minutes" -> initialTime = now.minusMinutes(time);
+                        case "hours" -> initialTime = now.minusHours(time);
+                        default -> throw new IllegalArgumentException("Invalid time type: " + type);
+                    }
 
-        switch (type) {
-            case "seconds" -> initialTime = now.minusSeconds(time);
-            case "minutes" -> initialTime = now.minusMinutes(time);
-            case "hours" -> initialTime = now.minusHours(time);
-            default -> throw new IllegalArgumentException("Invalid time type: " + type);
-        }
-
-        return statistic.statistic(initialTime);
+                    return statistic.statistic(initialTime);
+                });
     }
 }
