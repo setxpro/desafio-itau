cc908ad6920641f97b76102606b203957165c92b
Patrick Anjos <patrickpqdt87289@gmail.com>
2024-10-10 16:29:56 -0300

    config: added config to delete method

--- src/main/java/com/github/setxpro/challenge_itau/infra/configs/BeansConfig.java
+++ src/main/java/com/github/setxpro/challenge_itau/infra/configs/BeansConfig.java
@@ -3,15 +3,20 @@ package com.github.setxpro.challenge_itau.infra.configs;
 import com.github.setxpro.challenge_itau.application.gateways.TransactionGateway;
 import com.github.setxpro.challenge_itau.application.usecases.CreateTransactionUseCase;
 import com.github.setxpro.challenge_itau.application.usecases.CreateTransactionUseCaseImpl;
+import com.github.setxpro.challenge_itau.application.usecases.DeleteAllTransactionsUseCase;
+import com.github.setxpro.challenge_itau.application.usecases.DeleteAllTransactionsUseCaseImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeansConfig {
-
     @Bean
     public CreateTransactionUseCase createTransactionUseCase(TransactionGateway transactionGateway) {
         return new CreateTransactionUseCaseImpl(transactionGateway);
     }
 
+    @Bean
+    public DeleteAllTransactionsUseCase deleteAllTransactionsUseCase(TransactionGateway transactionGateway) {
+        return new DeleteAllTransactionsUseCaseImpl(transactionGateway);
+    }
 }
