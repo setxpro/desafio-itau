37d9d0d3229d48cfc62eaa56c0c6ac223965713f
Patrick Anjos <patrickpqdt87289@gmail.com>
2024-10-10 09:47:25 -0300

    feat: create project

---
+++ src/test/java/com/github/setxpro/challenge_itau/ChallengeItauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.setxpro.challenge_itau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeItauApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
