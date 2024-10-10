d3c73b62d9b3ab0c56729649b99fd18245d51c3c
Patrick Anjos <patrickpqdt87289@gmail.com>
2024-10-10 13:11:52 -0300

    feat: added logs

--- src/main/java/com/github/setxpro/challenge_itau/infra/gateways/TransactionRepositoryGateway.java
+++ src/main/java/com/github/setxpro/challenge_itau/infra/gateways/TransactionRepositoryGateway.java
@@ -4,9 +4,11 @@ import com.github.setxpro.challenge_itau.application.gateways.TransactionGateway
 import com.github.setxpro.challenge_itau.domain.entities.Transaction;
 import com.github.setxpro.challenge_itau.infra.mappers.EntityMapper;
 import com.github.setxpro.challenge_itau.infra.persistences.repositories.TransactionRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class TransactionRepositoryGateway implements TransactionGateway {
 
     private final TransactionRepository transactionRepository;
@@ -17,6 +19,9 @@ public class TransactionRepositoryGateway implements TransactionGateway {
 
     @Override
     public Transaction createTransaction(Transaction transaction) {
+
+        log.info("Savind a new transaction {}", transaction.getDateHour());
+
         return EntityMapper.getInstance().toTransactionDomainEntity(
                 transactionRepository.save(
                         EntityMapper.getInstance().toTransactionEntity(transaction)
