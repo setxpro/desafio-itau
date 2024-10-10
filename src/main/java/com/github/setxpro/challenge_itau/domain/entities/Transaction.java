d93442c3258f56691468ad4c9b2885fcfbdfaa14
Patrick Anjos <patrickpqdt87289@gmail.com>
2024-10-10 12:35:25 -0300

    fix: added id in entity domain

--- src/main/java/com/github/setxpro/challenge_itau/domain/entities/Transaction.java
+++ src/main/java/com/github/setxpro/challenge_itau/domain/entities/Transaction.java
@@ -4,16 +4,25 @@ import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
 public class Transaction {
-
+    private Long transactionId;
     private BigDecimal amount;
     private OffsetDateTime dateHour;
 
-    public Transaction(BigDecimal amount, OffsetDateTime dateHour) {
+    public Transaction(Long transactionId, BigDecimal amount, OffsetDateTime dateHour) {
+        this.transactionId = transactionId;
         this.amount = amount;
         this.dateHour = dateHour;
     }
 
     public Transaction() {
+    }
+
+    public Long getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(Long transactionId) {
+        this.transactionId = transactionId;
     }
 
     public BigDecimal getAmount() {
